46a09750edc24a7b8ad1ab2c3a5a9e7d7f667ac2
templatis <templatis@googlemail.com>
2015-03-24 19:38:44 +0000

    Basic Express routing and Jade templating

---
+++ app.js
@@ -0,0 +1,19 @@
+// Module dependencies
+var express = require('express');
+
+// Express settings
+var app = express();
+var jade = require('jade');
+
+// Template settings
+app.set('view engine', 'jade');
+app.set('views', './views');
+
+// Routes
+app.get('/', function (req,res,next){
+	res.render('partials/landing');
+});
+
+// Start the server
+app.listen(3000);
+console.log('Running on 3000');
